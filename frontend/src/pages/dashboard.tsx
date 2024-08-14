285acc86c522e4e0dd39c8b47108e6280623536d
Rishit Umesh Padagatti <rishitpadagatti@gmail.com>
2024-08-15 03:25:43 +0530

    Small changes to the dashboard

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,92 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { serverLocationAtom } from "../atom/atoms";
-import { useRecoilValue } from "recoil";
 import { PenIcon } from "../components/Dashboard/PenIcon";
-import { useNavigate } from "react-router-dom";
 import { Avatar } from "../components/Dashboard/Avatar";
-import { Blog, Tag } from "../interfaces/interface";
 import { profileInitials } from "../functions/profileInitials";
 import { TagsNavbar } from "../components/Dashboard/TagsNavbar";
 import mediumLogo from '/mediumLogo.svg'
 import { MainBlogComponent } from "../components/Dashboard/MainBlogComponent";
-
-function useTags(n: number): [Tag[], boolean] {
-  const [tags, setTags] = useState<Tag[]>([])
-  const [loading, setLoading] = useState<boolean>(true)
-  const serverLocation = useRecoilValue(serverLocationAtom)
-  const navigate = useNavigate()
-
-  const getData = async () => {
-    try {
-      const result = await axios.get(`${serverLocation}/tag/tags`, {
-        headers: {
-          "Authorization": localStorage.getItem("Authorization")
-        }
-      })
-      setTags(result.data.tags)
-    } catch (error) {
-      console.error("Failed to fetch data", error)
-      localStorage.clear()
-      navigate("/signuporsignin")
-      return;
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    const value = setInterval(() => {
-      getData()
-    }, n * 1000);
-
-    getData()
-
-    return () => {
-      clearInterval(value)
-    }
-  }, [n])
-
-  return [tags, loading]
-}
-
-function useBlogs(n: number): [Blog[], boolean] {
-  const [blogs, setBlogs] = useState<Blog[]>([])
-  const [loading, setLoading] = useState<boolean>(true)
-  const serverLocation = useRecoilValue(serverLocationAtom)
-  const navigate = useNavigate()
-
-  const getData = async () => {
-    try {
-      const result = await axios.get(`${serverLocation}/blog/bulk`, {
-        headers: {
-          "Authorization": localStorage.getItem("Authorization")
-        }
-      })
-      setBlogs(result.data.blogs)
-    } catch (error) {
-      console.error("Failed to fetch data", error)
-      localStorage.clear()
-      navigate("/signuporsignin")
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    const value = setInterval(() => {
-      getData()
-    }, n * 1000);
-
-    getData()
-
-    return () => {
-      clearInterval(value)
-    }
-  }, [n])
-
-  return [blogs, loading]
-}
 
 export default function Dashboard() {
   return (
